6aeff697efc758429afe8d6411c7f8ad061b1d47
LordAkirast <darkwarish@gmail.com>
2024-07-11 00:27:49 -0300

    feat: concatenacao de strings com java e print na tela

--- java_class/java-anatomia-classes/src/MinhaClasse.java
+++ java_class/java-anatomia-classes/src/MinhaClasse.java
@@ -1,8 +1,28 @@
 public class MinhaClasse {
 
    public static void main (String [] args) {
-    System.out.print("Olá! Tudo bem?");
+    System.out.print("Olá! Tudo bem?\n");
 
+    String meuNome = "Daniel";
+    String segundoNome = "Rosa";
+    int anoDeFabricacao = 2022;
+    boolean verdadeira = true;
+    anoDeFabricacao = 2018;
+    String nomeCompleto = nomeCompleto(meuNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
+    
+
+
+    ///tipo nomeDefinido = atribuição
+
+   }
+
+
+   ////retornar concatenação de strings
+   public static String nomeCompleto (String meuNome, String segundoNome) {
+    return "Resultado do Método: " + meuNome.concat(" ").concat(segundoNome);
    }
     
 }
