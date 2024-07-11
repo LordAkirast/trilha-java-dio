4cdbc72491e84e9a1b7132da48807843ca16ebeb
LordAkirast <darkwarish@gmail.com>
2024-07-11 00:37:12 -0300

    feat: identação de código e if e else

---
+++ java_class/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,11 @@
+public class BoletimEstudantil { ///classe
+    public static void main(String[] args) { ///metodo
+        int mediaFinal = 2; ///fluxo
+        if(mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal == 6)
+            System.out.println("PROVA DE MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
