bcf0fb1911d028b57081a85bc98fb87d1402efa5
LordAkirast <darkwarish@gmail.com>
2024-07-12 00:12:07 -0300

    feat: input de dados usando scanner

--- java-anatomia-classes/src/edu/daniel/argumentos/AboutMe.java
+++ java-anatomia-classes/src/edu/daniel/argumentos/AboutMe.java
@@ -1,13 +1,24 @@
 package edu.daniel.argumentos;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
 
-        System.out.println("Olá! Eu me chamo " + nome + " " + sobrenome + " Tenho " + idade + " anos e tenho " + altura + " de altura.");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Entre com o nome: ");
+        String nome = scanner.next();
+        
+        //String sobrenome = args[1];
+        //int idade = Integer.valueOf(args[2]);
+        System.out.println("Entre com a altura: ");
+        double altura = scanner.nextDouble();
+        
+        System.out.println("Meu nome é " + nome + " E minha altura é: " + altura);
+
+        //System.out.println("Olá! Eu me chamo " + nome + " " + sobrenome + " Tenho " + idade + " anos e tenho " + altura + " de altura.");
 
 
     }
