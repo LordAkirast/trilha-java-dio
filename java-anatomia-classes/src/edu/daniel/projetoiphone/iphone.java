86fdf20d575cbca53023eac14f1934c94828e5a2
LordAkirast <darkwarish@gmail.com>
2024-07-26 22:26:24 -0300

    first commit: projetoiphone

---
+++ java-anatomia-classes/src/edu/daniel/projetoiphone/iphone.java
@@ -0,0 +1,11 @@
+package edu.daniel.projetoiphone;
+
+public class iphone {
+
+    public static void main(String[] args) {
+        
+    }
+    //reprodutor musical: tocar, pausar, selecionar música
+    //aparelhoTelefonico: ligar, atender, iniciar correio de voz
+    //navegador na internet: exibirpagina, adicionarpagina, atualizarpagina
+}
